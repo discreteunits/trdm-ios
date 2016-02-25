eadf8184d8529833dde40c44c2ab60097a2ecb1b
Tobias Brysiewicz <tbrysiewicz@att.net>
2016-02-24 18:00:14 -0800

    Storyboard integration

--- TastingRoomDelMar/ENSideMenuControllerViewController.swift
+++ TastingRoomDelMar/ENSideMenuControllerViewController.swift
@@ -98,6 +98,7 @@ public extension UIViewController {
         } while (iteration != nil)
         
         return iteration as? ENSideMenuProtocol
+        
     }
     
     internal func topMostController () -> ENSideMenuProtocol? {
