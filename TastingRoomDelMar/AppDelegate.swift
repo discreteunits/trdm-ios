048885ae56876e3021d217331ae28a8c125881bd
buddybuild <help@buddybuild.com>
2016-04-22 02:08:48 -0700

    Adding buddybuild SDK
    - Add BuddyBuildSDK.framework
    - Initialize in App Delegate

--- TastingRoomDelMar/AppDelegate.swift
+++ TastingRoomDelMar/AppDelegate.swift
@@ -27,6 +27,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        BuddyBuildSDK.setup()
+        
         
         // Remove Auto Layout Constraint Errors From Log
         NSUserDefaults.standardUserDefaults().setValue(false, forKey: "_UIConstraintBasedLayoutLogUnsatisfiable")
