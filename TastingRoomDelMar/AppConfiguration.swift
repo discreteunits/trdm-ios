5db2d23e71051a2fcab2d86717a47a46fb7f473c
Andrew Wilhelm <andrewwilhelmex@gmail.com>
2016-05-19 23:26:14 -0700

    swapping development provisioning profiles and going back to strip test

--- TastingRoomDelMar/AppConfiguration.swift
+++ TastingRoomDelMar/AppConfiguration.swift
@@ -23,10 +23,10 @@ class AppConfiguration: NSObject {
     private var _CLIENT_ID = " "
     private var _SERVER_URL = "https://trdm-production-717.nodechef.com/parse"
     
-//    // Sand Box Dev
-//    private var _STRIPE_ID = "pk_test_Ks6cqeQtnXJN0MQIkEOyAmKn"
+    // Sand Box Dev
+    private var _STRIPE_ID = "pk_test_Ks6cqeQtnXJN0MQIkEOyAmKn"
     // Production
-    private var _STRIPE_ID = "pk_live_QXBEAVF6Pl18ZRcXngMr1NAr"
+    // private var _STRIPE_ID = "pk_live_QXBEAVF6Pl18ZRcXngMr1NAr"
     
     
     
